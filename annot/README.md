572ff02275b3e40cec65853551e1d43e536a096f
Benjamin Buchfink <buchfink@gmail.com>
2024-01-31 18:21:17 +0100

    Added annot benchmark

--- annot/README.md
+++ annot/README.md
@@ -2,4 +2,8 @@ Script for running MMseqs2 on the subset of Pfam-annotated sequences.
 
 Data files needed: `arch80_all.tsv`
 
+Software needed: MMseqs2, clusteval
+
 Setup: `./setup.sh`
+
+Run: `./run.sh`
--- annot/mmseqs.sh
+++ annot/mmseqs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mmseqs cluster annot out . --remove-tmp-files --threads 64 $?
+mmseqs cluster annot out . --remove-tmp-files --threads 64 "$@"
 mmseqs createtsv annot annot out out.tsv
 clusteval clust .. out.tsv > eval.tsv
-sens=`grep SENS eval.tsv | cut -f2,3 | ../util/convert-eval.py | ../util/avg.sh`
-prec=`grep PREC eval.tsv | cut -f2,3 | ../util/convert-eval.py | ../util/avg.sh`
-echo -e "$?\t$sens\t$prec"
+sens=`grep SENS eval.tsv | cut -f3,4 | ../util/convert-eval.py | ../util/avg.sh`
+prec=`grep PREC eval.tsv | cut -f3,4 | ../util/convert-eval.py | ../util/avg.sh`
+echo -e "$@\t$sens\t$prec"
 rm out*
