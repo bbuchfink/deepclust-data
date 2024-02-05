809c945ba72370b558f666a9c052515c876428f7
Benjamin Buchfink <buchfink@gmail.com>
2024-02-05 11:24:11 +0100

    updated eval

--- util/get-eval.sh
+++ util/get-eval.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clusteval clust .. $1 > eval.tsv
-sens=`grep SENS eval.tsv | cut -f3,4 | ../util/convert-eval.py | ../util/avg.sh`
-prec=`grep PREC eval.tsv | cut -f3,4 | ../util/convert-eval.py | ../util/avg.sh`
+grep SENS eval.tsv | cut -f3,4 | ../util/convert-eval.py > $2.sens.tsv
+grep PREC eval.tsv | cut -f3,4 | ../util/convert-eval.py > $2.prec.tsv
+sens=`cat $2.sens.tsv | ../util/avg.sh`
+prec=`cat $2.prec.tsv | ../util/avg.sh`
 echo -e "$2\t$sens\t$prec"
