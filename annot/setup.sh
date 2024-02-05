c449fe3f8ab184ae33a0a73ac22ce7114b931336
Benjamin Buchfink <buchfink@gmail.com>
2024-02-05 10:10:25 +0100

    Update setup.sh

--- annot/setup.sh
+++ annot/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt install seqtk
 wget https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nr.gz
 seqtk subseq nr.gz <(cut -f1 ../arch80_all.tsv) > annot.faa
 mmseqs createdb annot.faa annot
