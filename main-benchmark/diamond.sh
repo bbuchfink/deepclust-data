214fc897a287331b037aa190523d3520868d69fe
Benjamin Buchfink <buchfink@gmail.com>
2025-07-31 21:22:40 +0200

    updated parameters

--- main-benchmark/diamond.sh
+++ main-benchmark/diamond.sh
@@ -1,4 +1,4 @@
 /usr/bin/time diamond deepclust -d $1 -o out -M 2000G --mutual-cover 85 --swipe-task-size 10000000 \
         --anchored-swipe --connected-component-depth 1 1 3 3 3 3 -e 0.1 --cluster-steps faster_lin fast_lin \
-        fast default sensitive very-sensitive --no-reassign
+        fast default sensitive very-sensitive --no-reassign --round-approx-id 27 0 --round-coverage 87 85
 ../util/get-eval.sh out diamond
